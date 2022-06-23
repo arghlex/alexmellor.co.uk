ba25bbbc2ac5d6fcc341fd8262ca3bdc4f435457
Alex Mellor <alex.mellor@gmail.com>
2022-06-23 23:14:03 +0100

    - initial react app commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="site-header">
+        <h1>Alex Mellor</h1>
+        <ul className="sub-title">
+          <li>User Experience Design</li>
+          <li>User Interface Design &amp; Development</li>
+          </ul>
+      </header>
+    </>
+  );
+}
+
+export default App;
